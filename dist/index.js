ce405ad51eed47ef75352602b69c264db39ff52e
JuanTututi-Ksquare <Juan.Tututi@itksquare.edu.mx>
2022-05-15 11:54:03 -0500

    Project setup

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const index_1 = require("./models/index");
 dotenv_1.default.config();
-const server = (0, express_1.default)();
-server.use(express_1.default.json());
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const port = process.env.PORT;
-server.listen(port, () => {
-    console.log("Server is up at port:", port);
-});
+const db_name = process.env.DB_NAME;
+const db_username = process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
+const db_host = process.env.DB_HOST;
+app.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("app is up at port:", port);
+    try {
+        const connection = yield (0, index_1.DBConn)(db_name, db_username, db_password, db_host);
+        console.log("Connection to DB was succesful");
+    }
+    catch (error) {
+        console.log(error);
+    }
+}));
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import { DBConn } from "./models/index";
 
 dotenv.config();
 
-const server = express();
-server.use(express.json());
+const app = express();
+app.use(express.json());
 
 const port = process.env.PORT;
+const db_name = <string>process.env.DB_NAME;
+const db_username = <string>process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
+const db_host = process.env.DB_HOST;
 
-server.listen(port, () => {
-    console.log("Server is up at port:", port);
+app.listen(port, async () => {
+    console.log("app is up at port:", port);
+    try {
+        const connection = await DBConn(db_name, db_username, db_password, db_host);
+        console.log("Connection to DB was succesful")
+    } catch (error) {
+        console.log(error);
+    }
 })
