456bd7d6be021bbd06d1672a5c89b0abdb7f7b61
JuanTututi-Ksquare <Juan.Tututi@itksquare.edu.mx>
2022-05-19 19:22:01 -0500

    Doctors creation completed, Admins model created

--- src/handlers/CreateDoctor.handler.ts
+++ src/handlers/CreateDoctor.handler.ts
@@ -1,5 +1,7 @@
 import { Users } from "../models/Users.model";
 import { Doctors } from "../models/Doctors.model";
+import * as firebaseAdmin from "firebase-admin";
+import { Role } from "../config/CustomTypes";
 
 export const CreateDoctor = async (
   first_name: string,
@@ -9,21 +11,29 @@ export const CreateDoctor = async (
   id_area: number,
   email: string,
   password: string,
+  role: Role
 ) => {
-//   try {
-//     const userCreated = await Users.create({
-//       id: id,
-//       first_name: first_name,
-//       last_name: last_name,
-//       birthdate: birthdate,
-//     });
-//     await Doctors.create({
-//       id_user: userCreated.id,
-//       license: license,
-//       id_area: id_area,
-//     });
-//     return "Doctor registration was succesful!";
-//   } catch (error) {
-//     return error;
-//   }
+  try {
+    const firebaseUser = await firebaseAdmin.auth().createUser({
+      displayName: `${first_name} ${last_name}`,
+      email: email,
+      password: password,
+    });
+    await firebaseAdmin.auth().setCustomUserClaims(firebaseUser.uid, { role });
+    
+    const userCreated = await Users.create({
+      id: firebaseUser.uid,
+      first_name: first_name,
+      last_name: last_name,
+      birthdate: birthdate,
+    });
+    await Doctors.create({
+      id_user: userCreated.id,
+      license: license,
+      id_area: id_area
+    });
+    return firebaseUser.uid;
+  } catch (error) {
+    return error;
+  }
 };
